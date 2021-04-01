2f4bb7f287a8aadee01f185c15678e61ac4050d5
pk <4514654+pkrasam@users.noreply.github.com>
2021-04-01 14:35:36 +1100

    09

---
+++ scripts/00-governance/09-ag-cosmos-helper-tx-staking-create-validator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ag-cosmos-helper tx staking create-validator \
+  --amount=50000000uagstake \
+  --broadcast-mode=block \
+  --pubkey=$(ag-chain-cosmos tendermint show-validator) \
+  --moniker="<moniker>" \
+  --identity="<keybase>" \
+  --details="<details>" \
+  --commission-rate="0.1" \
+  --commission-max-rate="0.1" \
+  --commission-max-change-rate="0.1" \
+  --min-self-delegation="1" \
+  --from="<moniker>" \
+  --chain-id=$chainName \
+  --gas=auto \
+  --gas-adjustment=1.4 \
+  --node tcp://127.0.0.1:<port>
